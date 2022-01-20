5a1d170b6e179a685e993519ecc32a49648a6608
Andrew <lavatree05@gmail.com>
2022-01-19 16:21:24 -0800

    Latching added
    Added serve latching functions to climber

--- src/main/java/org/team1515/botifypremium/Subsystems/Climber.java
+++ src/main/java/org/team1515/botifypremium/Subsystems/Climber.java
@@ -2,28 +2,43 @@ package org.team1515.botifypremium.Subsystems;
 
 import org.team1515.botifypremium.RobotMap;
 
+import edu.wpi.first.wpilibj.Servo;
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Climber {
 
     private CANSparkMax m_climber;
+    private static Servo latcher;
+
     private final double c_speed = 0.25;
+    private final double closePos = 0.5;
+    private final double startPos = 0.0;
 
-
-    public Climber(){
+    public Climber() {
 
         m_climber = new CANSparkMax(RobotMap.CLIMBER_ID, MotorType.kBrushless);
         m_climber.restoreFactoryDefaults();
 
+        latcher.set(startPos);
+
     }
 
-    public void climb(){
+    public void climb() {
         m_climber.set(c_speed);
     }
 
-    public void retract(){
+    public void retract() {
         m_climber.set(-c_speed);
+    }
+
+    public void latch() {
+        latcher.set(closePos);
+    }
+
+    public void unlatch() {
+        latcher.set(startPos);
     }
 
 }
