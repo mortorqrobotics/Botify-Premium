d91689985c9a056fd3849f45e452b4acad856258
Andrew <lavatree05@gmail.com>
2022-01-19 16:40:45 -0800

    Fixed controls
    Fixed climber controls

--- src/main/java/org/team1515/botifypremium/Controls.java
+++ src/main/java/org/team1515/botifypremium/Controls.java
@@ -5,4 +5,12 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 public class Controls {
     private static final int A_BUTTON = 1;
     public static final JoystickButton SHOOT = new JoystickButton(OI.mainStick, A_BUTTON);
+
+    private static final int R_BUTTON = 8;
+    public static final JoystickButton EXTEND = new JoystickButton(OI.mainStick, R_BUTTON);
+
+    private static final int L_BUTTON = 7;
+    public static final JoystickButton RETRACT = new JoystickButton(OI.mainStick, L_BUTTON);
+
+
 }
