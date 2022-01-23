c1c6a436d43050d8d0f3aea16e00bf97d6367c0a
Cameron <meme34903@gmail.com>
2022-01-23 09:54:48 -0800

    fix drivetrain max velocity calculation

--- src/main/java/org/team1515/botifypremium/Subsystems/Drivetrain.java
+++ src/main/java/org/team1515/botifypremium/Subsystems/Drivetrain.java
@@ -40,8 +40,8 @@ public class Drivetrain extends SubsystemBase {
    * This is a measure of how fast the robot should be able to drive in a straight line.
    */
   public static final double MAX_VELOCITY_METERS_PER_SECOND = 6380.0 / 60.0 *
-          SdsModuleConfigurations.MK3_STANDARD.getDriveReduction() *
-          SdsModuleConfigurations.MK3_STANDARD.getWheelDiameter() * Math.PI;
+          SdsModuleConfigurations.MK4_L2.getDriveReduction() *
+          SdsModuleConfigurations.MK4_L2.getWheelDiameter() * Math.PI;
   /**
    * The maximum angular velocity of the robot in radians per second.
    * <p>
