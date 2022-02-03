d29fa2990dcbf8d5ac1a0845a3e5bd1b3f1ce3cf
Hayley <hayley.chechel@gmail.com>
2022-02-02 16:37:55 -0800

    add

---
+++ src/main/java/org/team1515/botifypremium/Subsystems/Magazine.java
@@ -0,0 +1,32 @@
+package org.team1515.botifypremium.Subsystems;
+ 
+//import org.team1515.botifypremium.Robot;
+import org.team1515.botifypremium.RobotMap;
+//import org.team1515.botifypremium.Utils.ShooterDist;
+ 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+ 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+ 
+public class Magazine extends SubsystemBase{
+    private CANSparkMax m_mag;
+    private final double speed = 0.5;
+ 
+    public Magazine() {
+        m_mag = new CANSparkMax(RobotMap.MAG_ID, MotorType.kBrushless);
+        m_mag.restoreFactoryDefaults();
+    }
+ 
+    public void goUp() {
+        m_mag.set(speed);
+    }
+ 
+    public void goDown() {
+        m_mag.set(-speed);
+    }
+ 
+    public void end() {
+        m_mag.set(0.0);
+    }
+}
