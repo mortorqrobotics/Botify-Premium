a8d9bfe15183f63a9e841a2bd4ff52d34fa324a5
Daniel <daniel@walden.vc>
2022-02-06 15:24:38 -0800

    Update BallVision.java

---
+++ src/main/java/org/team1515/botifypremium/Utils/BallVision.java
@@ -0,0 +1,20 @@
+package org.team1515.botifypremium.Utils;i
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+public class BallVision {
+    
+    NetworkTable table;
+    public static double distance;
+    public static double offset;
+
+
+
+    public BallVision(){
+        table = NetworkTableInstance.getDefault().getTable("FRCMap");
+    }
+
+    public void GetValues(){
+        distance = table.getEntry("distance").getDouble(9999.0);
+        offset = table.getEntry("offset").getDouble(9999.0);
+    }
+}
