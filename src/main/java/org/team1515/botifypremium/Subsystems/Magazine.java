f503c4c3c37e5793f34d0699bfafbfce76bc44c0
Andrew <lavatree05@gmail.com>
2022-02-10 16:45:54 -0800

    Added ultrasonic mag restrictions

--- src/main/java/org/team1515/botifypremium/Subsystems/Magazine.java
+++ src/main/java/org/team1515/botifypremium/Subsystems/Magazine.java
@@ -1,7 +1,8 @@
 package org.team1515.botifypremium.Subsystems;
  
 import org.team1515.botifypremium.RobotMap;
- 
+import org.team1515.botifypremium.Utils.UltraSensor;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
  
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
@@ -10,14 +11,18 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 public class Magazine extends SubsystemBase{
     private TalonFX m_mag;
     private final double speed = 0.5;
+    private final UltraSensor ultraSensor;
  
     public Magazine() {
         m_mag = new TalonFX(RobotMap.MAG_ID);
         m_mag.configFactoryDefault();
         m_mag.configNeutralDeadband(0.001);
+
+        ultraSensor = new UltraSensor();
     }
  
     public void goUp() {
+        if (ultraSensor.itemDetected()) 
         m_mag.set(ControlMode.PercentOutput, speed);
     }
  
