e47f2c57d2a0bfe407f147a2f8d9d4a39ff5616b
Cameron <meme34903@gmail.com>
2022-02-12 11:32:19 -0800

    glass and fix style

--- src/main/java/org/team1515/botifypremium/Utils/BallVision.java
+++ src/main/java/org/team1515/botifypremium/Utils/BallVision.java
@@ -1,18 +1,15 @@
 package org.team1515.botifypremium.Utils;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
+
 public class BallVision {
-    
     NetworkTable table;
     public static double distance;
     public static double offset;
 
-
-
     public BallVision(){
         table = NetworkTableInstance.getDefault().getTable("FRCMap");
     }
-
 
     public void GetValues(){
         distance = table.getEntry("distance").getDouble(9999.0);
