f32553098d4e0c676425da4cfaa9d0147253675a
Turquoisee <daniel@walden.vc>
2022-03-01 17:45:25 -0800

    updated retract

--- src/main/java/org/team1515/botifypremium/Commands/Climber/Retract.java
+++ src/main/java/org/team1515/botifypremium/Commands/Climber/Retract.java
@@ -5,27 +5,38 @@ import org.team1515.botifypremium.Subsystems.Climber;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class Retract extends CommandBase {
-    Climber climber;
+    Climber climber1;
+    Climber climber2;
 
     public Retract(Climber climber1, Climber climber2) {
-        this.climber = climber;
+        this.climber1 = climber1;
+        this.climber2 = climber2;
 
-        addRequirements(climber);
+        addRequirements(climber1);
+        addRequirements(climber2);
     }
 
     @Override
     public void execute() {
-        climber.retract();
+        climber1.retract();
+        climber2.retract();
     }
 
     @Override
     public void end(boolean interrupted) {
-        climber.end();
+        climber1.end();
+        climber2.end();
     }
 
-    // @Override
-    // public boolean isFinished() {
-    //     // Stops when the climber reaches the minimum distance
-    //     return climber.stringPot.getDist() <= climber.minDist;
-    // }
+    @Override
+    public boolean isFinished() {
+        // Stops when the climber reaches the maximum distance
+        if(climber1.stringPot.getDist() <= climber1.minDist) {
+            return true;
+        }
+        if(climber2.stringPot.getDist() <= climber2.minDist) {
+            return true;
+        }
+        return false;
+    }
 }
