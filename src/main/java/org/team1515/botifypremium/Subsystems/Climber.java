669077f5432c888f0a94bc6b7e8f7d454fd24af2
Andrew <lavatree05@gmail.com>
2022-03-19 10:23:28 -0700

    Changed climber to falcon

--- src/main/java/org/team1515/botifypremium/Subsystems/Climber.java
+++ src/main/java/org/team1515/botifypremium/Subsystems/Climber.java
@@ -6,7 +6,10 @@ import org.team1515.botifypremium.Utils.StringPot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import com.revrobotics.CANSparkMax;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMax.IdleMode;
@@ -14,17 +17,17 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Climber extends SubsystemBase {
 
-    private CANSparkMax m_climber;
+    private TalonFX m_climber;
     private RelativeEncoder m_encoder;
-    public StringPot stringPot;
+    // public StringPot stringPot;
     public SparkMaxPIDController m_pidController;
 
     private int climberID;
-    private int stringPotID;
+    // private int stringPotID;
     private int direction;
 
-    public final double maxDist = 68.58; //27 inches, max extension of climber
-    public final double minDist = 5.00;
+    // public final double maxDist = 68.58; //27 inches, max extension of climber
+    // public final double minDist = 5.00;
     private final double c_speed = 0.25;
     public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput, allowedErr;
     private double setPoint = 0;
@@ -32,30 +35,23 @@ public class Climber extends SubsystemBase {
 
     public Climber(int climberID, int stringPotID, int direction) {
         this.climberID = climberID;
-        this.stringPotID = stringPotID;
+        // this.stringPotID = stringPotID;
         this.direction = direction;
 
-        m_climber = new CANSparkMax(climberID, MotorType.kBrushless);
-        m_climber.restoreFactoryDefaults();
-        m_climber.setIdleMode(IdleMode.kBrake);
+        m_climber = new TalonFX(climberID);
+        m_climber.configFactoryDefault();
+        m_climber.setNeutralMode(NeutralMode.Brake);
 
-        m_encoder = m_climber.getEncoder();
-        m_pidController = m_climber.getPIDController();
+        m_climber.configNeutralDeadband(0.001);
+        m_climber.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor,
+                CPIDMap.kPIDLoopIdx,
+                CPIDMap.kTimeoutMs);
    
         // stringPot = new StringPot(stringPotID);
         // reset();
 
-        m_encoder.setPosition(0);
+        m_climber.setSelectedSensorPosition(0);
 
-        this.kP = 0.06; 
-        this.kI = 0.0003;
-        this.kD = 0; 
-        this.kIz = 0; 
-        this.kFF = 0; 
-        this.kMaxOutput = 1; 
-        this.kMinOutput = -1;
-
-        setPID();
     }
 
     // public void reset(){
@@ -73,20 +69,36 @@ public class Climber extends SubsystemBase {
     }
 
     public void climberPeriodic() {
-        m_pidController.setReference(setPoint * direction, CANSparkMax.ControlType.kPosition);
+        m_climber.set(ControlMode.Position, setPoint * direction);
         processVariable = m_encoder.getPosition(); 
-    }
-
-    public void setPID() {
-        m_pidController.setP(this.kP);
-        m_pidController.setI(this.kI);
-        m_pidController.setD(this.kD);
-        m_pidController.setIZone(this.kIz);
-        m_pidController.setFF(this.kFF);
-        m_pidController.setOutputRange(this.kMinOutput, this.kMaxOutput);
     }
 
     public double getPosition() {
         return processVariable;
     }
+
+    public void setPID(){
+        m_climber.configNominalOutputForward(0, CPIDMap.kTimeoutMs);
+        m_climber.configNominalOutputReverse(0, CPIDMap.kTimeoutMs);
+        m_climber.configPeakOutputForward(1, CPIDMap.kTimeoutMs);
+        m_climber.configPeakOutputReverse(-1, CPIDMap.kTimeoutMs);
+
+        m_climber.config_kF(CPIDMap.kPIDLoopIdx, CPIDMap.kF, CPIDMap.kTimeoutMs);
+        m_climber.config_kP(CPIDMap.kPIDLoopIdx, CPIDMap.kP, CPIDMap.kTimeoutMs);
+        m_climber.config_kI(CPIDMap.kPIDLoopIdx, CPIDMap.kI, CPIDMap.kTimeoutMs);
+        m_climber.config_kD(CPIDMap.kPIDLoopIdx, CPIDMap.kD, CPIDMap.kTimeoutMs);
+    }
+}
+
+class CPIDMap {
+    public static double kP = 0.06;
+    public static double kI = 0.0003;
+    public static double kD = 0;
+    public static double kF = 0;
+    public static double iZone = 0;
+    public static double peakOutput = 1.00;
+
+    public static final int kPIDLoopIdx = 0;
+    public static final int kTimeoutMs = 30;
+    public static boolean kSensorPhase = true;
 }
